a2c743296cebdeeb4322dc1505bbf84251ed8e5a
furkan bora <frknbora@hotmail.com>
2023-11-20 03:03:01 +0300

    just added a func

--- bin/auto_tweet.dart
+++ bin/auto_tweet.dart
@@ -1,5 +1,7 @@
 import 'package:auto_tweet/auto_tweet.dart' as auto_tweet;
+import "package:auto_tweet/gpt.dart" as gpt;
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async {
   print('Hello world: ${auto_tweet.calculate()}!');
+  print(await gpt.makeTweet(send: "test"));
 }
--- lib/gpt.dart
+++ lib/gpt.dart
@@ -1,15 +1,22 @@
+import 'dart:convert';
+
+import 'package:auto_tweet/secret.dart';
 import 'package:http/http.dart' as http;
 
-void post({required String send}) {
-  http.post(Uri.parse('uri'), headers: <String, String>{
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer turkcekaraktersevilmiyor'
-  }, body: {
-    "model": "gpt-3.5-turbo",
-    "messages": [
-      {"role": "system", "content": send},
-      {"role": "user", "content": send}
-    ],
-    "temperature": 0.5
-  });
+Future makeTweet({required String send}) async {
+  http.Response response = await http.post(
+      Uri.parse('https://api.openai.com/v1/chat/completions'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $bererToekn'
+      },
+      body: jsonEncode({
+        "model": "gpt-3.5-turbo",
+        "messages": [
+          {"role": "system", "content": ''},
+          {"role": "user", "content": send}
+        ],
+        "temperature": 0.5
+      }));
+  return jsonDecode(utf8.decode(response.bodyBytes));
 }
