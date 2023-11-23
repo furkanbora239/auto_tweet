2f60349d31b7997cb3d91a86981c9f69c223207d
furkan bora <frknbora@hotmail.com>
2023-11-23 04:23:51 +0300

    screp is done sayılır

--- bin/auto_tweet.dart
+++ bin/auto_tweet.dart
@@ -5,5 +5,5 @@ import 'package:auto_tweet/scraper.dart';
 void main(List<String> arguments) async {
   print(await T24().haberDetay(
       link: Uri.parse(
-          'https://t24.com.tr/haber/avrupa-da-elektrikliler-haric-kamyon-ve-otobusler-yasaklaniyor,1139930')));
+          'https://t24.com.tr/yazarlar/bulent-yuceturk/anayasa-mahkemesi-bahane-50-1-i-kaldirmak-sahane,42368')));
 }
--- lib/scraper.dart
+++ lib/scraper.dart
@@ -24,6 +24,7 @@ class T24 {
     late String date;
     late String title;
     String? subtitle;
+    late final String t24Category;
     final http.Response response = await http.get(link);
     final Document html = Document.html(utf8.decode(response.bodyBytes));
     final String croppedLink =
@@ -31,6 +32,7 @@ class T24 {
     String content = '';
     List mediaLinks = [];
     if (croppedLink.startsWith('video')) {
+      t24Category = 'video';
       date = html
           .querySelector(
               "div._3mmXU > div._1XNyq > div._2l0_f > div > div._392lz > p")!
@@ -45,6 +47,8 @@ class T24 {
         content += element.text;
       }
     } else if (croppedLink.startsWith('haber')) {
+      t24Category = 'haber';
+
       date = html
           .querySelector(
               "div._1d1Rj > div > div._2l0_f > div > div._392lz > p")!
@@ -53,27 +57,67 @@ class T24 {
           .querySelector("div.col-md-8.col-xs-12 > div > div.d0FP9 > h1")!
           .text;
       subtitle = html
-          .querySelector("div.col-md-8.col-xs-12 > div > div.d0FP9 > h2")!
-          .text;
+          .querySelector("div.col-md-8.col-xs-12 > div > div.d0FP9 > h2")
+          ?.text;
+
       final contentData =
           html.querySelectorAll('div._3QVZl > div:not(script)').toList();
       for (var element in contentData) {
         content += element.text;
         content += '\n';
       }
-      final media = html.querySelectorAll('div._2hwRY > img._3xXvK');
+      final media = html.querySelectorAll('div._2hwRY > img');
       for (var element in media) {
         mediaLinks.add(element.attributes['src']);
       }
-      print(media);
     } else if (croppedLink.startsWith('foto-haber')) {
-    } else if (croppedLink.startsWith('yazarlar')) {}
+      t24Category = 'foto-haber';
+
+      title =
+          html.querySelector('div._1YxhA > div._1C396 > div._1VAN- > h1')!.text;
+      subtitle =
+          html.querySelector('div._1YxhA > div._1C396 > div._1VAN- > h2')?.text;
+      date =
+          html.querySelector('div._1YxhA > div._1C396 > div._1VAN- > p')!.text;
+      final contentData =
+          html.querySelectorAll('div._39i_e > div > div > h3').toList();
+      for (var element in contentData) {
+        content += element.text;
+        content += '\n';
+      }
+    } else if (croppedLink.startsWith('yazarlar')) {
+      t24Category = 'yazarlar';
+
+      date = html.querySelector(' div._2PzRZ > div._392lz > p')!.text;
+      title = html
+          .querySelector('div.col-md-8.col-sm-12.col-xs-12 > div._2teaB > h1')!
+          .text;
+      subtitle = html
+          .querySelector('div.col-md-8.col-sm-12.col-xs-12 > div._2teaB > h2')!
+          .text;
+      final contentData = html.querySelectorAll(
+          'div.col-md-8.col-sm-12.col-xs-12 > div._2teaB > div._1NMxy > div > p');
+      for (var element in contentData) {
+        content += element.text;
+        content += '\n';
+      }
+
+      final media = html.querySelectorAll(
+          'div.col-md-8.col-sm-12.col-xs-12 > div._2teaB > div._1NMxy > div > h5 > img');
+      for (var element in media) {
+        mediaLinks.add(element.attributes['src']);
+      }
+    }
+    if (subtitle != null && subtitle.isEmpty) {
+      subtitle = null;
+    }
     return {
       'date': date,
       'title': title,
       'subtitle': subtitle,
       'content': content.trim(),
-      'mediaLinks': mediaLinks
+      'mediaLinks': mediaLinks,
+      'T24Category': t24Category
     };
   }
 }
