e8f0389b3a61fa839640ce393359e3405eeebf11
furkan bora <frknbora@hotmail.com>
2023-11-24 03:54:20 +0300

    get lest ten news is done

--- bin/auto_tweet.dart
+++ bin/auto_tweet.dart
@@ -1,9 +1,9 @@
 import 'package:auto_tweet/auto_tweet.dart' as auto_tweet;
+import 'package:auto_tweet/gdocs.dart';
 import "package:auto_tweet/gpt.dart" as gpt;
 import 'package:auto_tweet/scraper.dart';
 
 void main(List<String> arguments) async {
-  print(await T24().haberDetay(
-      link: Uri.parse(
-          'https://t24.com.tr/yazarlar/bulent-yuceturk/anayasa-mahkemesi-bahane-50-1-i-kaldirmak-sahane,42368')));
+  print(await GSheetsApi().init());
+  print(await GSheetsApi().getLestTenNews());
 }
