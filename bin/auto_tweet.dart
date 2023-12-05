d496cc9c4967fa53726b7ddf9ec4920a7e0d230c
furkan bora <frknbora@hotmail.com>
2023-12-05 04:41:50 +0300

    working good bug fix

--- bin/auto_tweet.dart
+++ bin/auto_tweet.dart
@@ -5,7 +5,7 @@ import 'package:auto_tweet/scraper.dart';
 
 void main(List<String> arguments) async {
   print(await GSheetsApi().init());
-  var sonDakika = await T24().sonDakika();
+  /* var sonDakika = await T24().sonDakika();
   var lastTenNews = await GSheetsApi().getLestTenNews();
   for (int i = 0; i < lastTenNews.length; i++) {
     for (int s = 0; s < sonDakika.length; s++) {
@@ -30,5 +30,47 @@ void main(List<String> arguments) async {
     GSheetsApi().write(
         worksheet: GSheetsApi.t24SonDakikaWorkSheet,
         data: saveSonDakika.reversed.toList());
+  } */
+  saveNewNews();
+  /* print(await T24().haberDetay(
+      link: Uri.parse(
+          'https://t24.com.tr/haber/antalya-ucusunda-yanindaki-yolcuya-zorla-oral-seks-yapan-kadin-gozaltina-alindi,1140960'))); */
+}
+
+void saveNewNews() async {
+  var sonDakika = await T24().sonDakika();
+  var lastTenNews = await GSheetsApi().getLestTenNewsT24();
+  comparison:
+  for (int i = 0; i < lastTenNews.length; i++) {
+    for (int s = 0; s < sonDakika.length; s++) {
+      print(
+          'başlık taranıyor ${lastTenNews[i][2]} == ${sonDakika[s]['title']}');
+      if (lastTenNews[i][2] == sonDakika[s]['title']) {
+        sonDakika.removeRange(s, sonDakika.length);
+        print('benzer başlık bulundu');
+        break comparison;
+      }
+    }
+  }
+  List<List<dynamic>> saveSonDakika = [];
+  if (sonDakika.isNotEmpty) {
+    for (var element in sonDakika) {
+      print('yeni başlıklar listeye çevriliyor $element');
+      saveSonDakika.add([
+        '',
+        element["time"].toString(),
+        element["title"].toString(),
+        element["link"].toString()
+      ]);
+      saveSonDakika.last.insertAll(saveSonDakika.last.length,
+          await gpt.tagger(title: element['title'].toString()));
+    }
+  }
+  if (saveSonDakika.isNotEmpty) {
+    saveSonDakika.first.first = DateTime.now().toIso8601String();
+    GSheetsApi().write(
+        worksheet: GSheetsApi.t24SonDakikaWorkSheet,
+        data: saveSonDakika.reversed.toList());
+    print('yeni başlıklar docs a kaydedildi');
   }
 }
